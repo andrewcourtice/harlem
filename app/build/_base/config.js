9a9c7cb6e216576ec32ba6fcbb6619b0bfc8d8be
Andrew Courtice <andrewcourtice@users.noreply.github.com>
2020-10-05 22:11:34 +1000

    fix(app): changed output to suit vercel deployment

--- app/build/_base/config.js
+++ app/build/_base/config.js
@@ -19,7 +19,7 @@ export default {
     },
 
     output: {
-        path: path.resolve(__dirname, '../../dist'),
+        path: path.resolve(__dirname, '../../public'),
         publicPath: '/',
     },
 
