fe4d8a0d1648f5f1fb6206fb2fcdaa6d9aa86715
Andrew Courtice <andrewcourtice@users.noreply.github.com>
2020-10-08 16:43:49 +1000

    enhancement(plugins): started basic storage plugin

--- plugins/storage/src/index.ts
+++ plugins/storage/src/index.ts
@@ -2,16 +2,53 @@ import type {
     HarlemPlugin
 } from '@harlem/core';
 
-export default function(): HarlemPlugin {
+import type {
+    Options,
+    StorageMap
+} from './types';
+
+const OPTIONS: Options = {
+    type: 'local',
+    prefix: 'harlem'
+};
+
+const STORAGE: StorageMap = {
+    local: localStorage,
+    session: sessionStorage
+};
+
+export default function(options: Options = OPTIONS): HarlemPlugin {
+    const {
+        type,
+        prefix
+    } = {
+        ...OPTIONS,
+        ...options
+    };
+
+    const storage = STORAGE[type] || STORAGE.local;
 
     return {
 
         name: 'storage',
 
-        install(app, eventEmitter) {
-            
+        install(app, eventEmitter, stores) {
+            const storageHook = (storeName: string) => {
+                const store = stores.get(storeName);
+        
+                if (!store) {
+                    return;
+                }
+        
+                const state = store.state();
+                const key = prefix ? `${prefix}:${storeName}` : storeName;
+        
+                storage.setItem(key, JSON.stringify(state));
+            };
+
+            eventEmitter.on('mutation', storageHook);
         }
 
-    }
+    };
 
-} 
+};
