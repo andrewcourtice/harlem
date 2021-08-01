c586e6a707f5149d89170a1e7000e8630b5fb0e8
Andrew Courtice <andrewcourtice@users.noreply.github.com>
2021-08-01 13:11:49 +1000

    feat(utilities): added task

--- packages/utilities/src/index.ts
+++ packages/utilities/src/index.ts
@@ -1,2 +1,6 @@
+export { default as Task } from './task';
+
 export { default as clone } from './clone';
 export { default as overwrite } from './overwrite';
+
+export * from './types';
--- packages/utilities/src/types.ts
+++ packages/utilities/src/types.ts
@@ -1,3 +1,5 @@
+export type Product<TResult = void> = (...args: any[]) => TResult;
+
 export type RuntimeType = 'boolean'
     | 'number'
     | 'string'
@@ -12,3 +14,12 @@ export type RuntimeType = 'boolean'
     | 'set'
     | 'null'
     | 'undefined';
+
+export type TaskAbortCallback = (reason?: any) => void;
+
+export type TaskExecutor<T> = (
+    resolve: (value: T | PromiseLike<T>) => void,
+    reject: (reason?: any) => any,
+    controller: AbortController,
+    onAbort: (callback: TaskAbortCallback) => void
+) => void;
