40fb44a2c9bbfb1f9d8a72f36c2135e913830f1d
Andrew Courtice <andrewcourtice@users.noreply.github.com>
2021-09-06 09:38:55 +1000

    chore(extensions): rmeoved unused types

--- extensions/action/src/types.ts
+++ extensions/action/src/types.ts
@@ -10,7 +10,6 @@ import Task, {
 export type ActionBody<TState extends BaseState, TPayload = undefined, TResult = void> = (payload: TPayload, mutator: (mutate: Mutator<TState, undefined, void>) => void, controller: AbortController, onAbort: (callback: TaskAbortCallback) => void) => Promise<TResult>;
 export type Action<TPayload, TResult = void> = undefined extends TPayload ? (payload?: TPayload, controller?: AbortController) => Task<TResult> : (payload: TPayload, controller?: AbortController) => Task<TResult>;
 export type ActionPredicate<TPayload = unknown> = (payload?: TPayload) => boolean;
-export type ComposedAction<TPayload, TResult = void> = [Action<TPayload, TResult>, (predicate: ActionPredicate<TPayload>) => boolean];
 
 export interface ActionTaskState {
     runCount: number;
