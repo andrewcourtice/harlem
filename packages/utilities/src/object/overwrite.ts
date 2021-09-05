9d8e71bdc762f54b12392c5abe8c49901d10f479
Andrew Courtice <andrewcourtice@users.noreply.github.com>
2021-09-06 09:39:51 +1000

    feat(utilities): added top-level ignore pattern to overwrite

--- packages/utilities/src/object/overwrite.ts
+++ packages/utilities/src/object/overwrite.ts
@@ -1,10 +1,12 @@
-export default function overwrite<TTarget extends object, TSource extends object>(target: TTarget, source: TSource): TTarget | TSource {
+export default function overwrite<TTarget extends object, TSource extends object>(target: TTarget, source: TSource, ignorePattern?: RegExp): TTarget | TSource {
     if (typeof target !== 'object' || typeof source !== 'object') {
         return target;
     }
 
     for (const prop in target) {
-        delete target[prop];
+        if (!ignorePattern || !ignorePattern.test(prop)) {
+            delete target[prop];
+        }
     }
 
     return Object.assign(target, source);
--- packages/utilities/test/overwrite.test.ts
+++ packages/utilities/test/overwrite.test.ts
@@ -28,6 +28,28 @@ describe('Utilities', () => {
             expect(source.e).toBeUndefined();
         });
 
+        test('Should handle a top-level ignore pattern', () => {
+            const source = {
+                a: 1,
+                b: null,
+                c: 'Hello',
+                d: 26,
+                e: [1, 2, 3],
+            };
+
+            overwrite(source, {
+                a: 'hello',
+                b: 5,
+                c: [1, 2],
+            }, /^d/);
+
+            expect(typeof source.a).toBe('string');
+            expect(typeof source.b).toBe('number');
+            expect(Array.isArray(source.c)).toBe(true);
+            expect(source.d).toBe(26);
+            expect(source.e).toBeUndefined();
+        });
+
     });
 
 });
