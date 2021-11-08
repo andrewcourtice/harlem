90086f446b83f03cae12bad29c13698ccc179a96
Andrew Courtice <andrewcourtice@users.noreply.github.com>
2021-11-08 10:43:00 +1000

    feat(utilities): added is-nil utility

---
+++ packages/utilities/test/type/is-nil.test.ts
@@ -0,0 +1,18 @@
+import isNil from '../../src/type/is-nil';
+
+describe('Utilities', () => {
+
+    describe('Type Is Nil', () => {
+
+        test('Should verify that a given type is nil', () => {
+            expect(isNil(undefined)).toBe(true);
+            expect(isNil(null)).toBe(true);
+            expect(isNil(void 0)).toBe(true);
+            expect(isNil(NaN)).toBe(false);
+            expect(isNil(false)).toBe(false);
+            expect(isNil(0)).toBe(false);
+        });
+
+    });
+
+});
