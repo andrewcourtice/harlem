e79beea8c1f6f2cb686f6a3979a08e2c9c2b58c8
Andrew Courtice <andrewcourtice@users.noreply.github.com>
2022-02-26 17:24:47 +1000

    feat(examples): added nuxt 3 demo

---
+++ examples/nuxt/store.ts
@@ -0,0 +1,22 @@
+import composeExtension from '@harlem/extension-compose';
+import storageExtension from '@harlem/extension-storage';
+import resetExtension from '@harlem/extension-reset';
+
+export const {
+    state,
+    getter,
+    reset,
+    computeState,
+} = createStore('app', {
+    firstName: 'John',
+    lastName: 'Smith',
+}, {
+    extensions: [
+        composeExtension(),
+        storageExtension({
+            prefix: 'nuxt',
+            restore: true,
+        }),
+        resetExtension(), // order is important!
+    ],
+});
